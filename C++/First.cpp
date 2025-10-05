8ffbc18b635a6b8553ec6c18a8fa92d8edc84b5d
Pallaviisable <pallaviisable@gmail.com>
2025-10-05 12:58:36 +0530

    Add files via upload
    This folders demonstrates a simple C++ programs that illustrates concepts according to the name of the file. The file contains a description of the program, which helps in understanding the following basic C++ concepts:
    
    Preprocessor Directive (#)
    Used to include header files before compilation.
    #include <iostream> includes the Input/Output stream library, which provides objects like cout and cin.
    
    cout,cin
    Used to display output on the console and take input from Keyboard.
    
    using namespace std;
    Allows the program to use standard library names like cout and cin directly without prefixing them with std::.
    
    Block-Structured Language
    C++ is a block-structured language, meaning that the code is organized into functions and blocks using braces { }.
    
    main() function
    The entry point of every C++ program where execution starts.

---
+++ C++/First.cpp
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////
+//
+//  File name :     First.cpp
+//  Description :   This program demonstrates a simple C++ output
+//                  using cout to display a "Jay Ganesh" on the screen.
+//  Author :        Pallavi Sable
+//  Date :          05/10/2025
+//
+///////////////////////////////////////////////////////
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Jay Ganesh...\n";
+
+    return 0;
+}
